4b938cdc783cd5beee01a4663469c2fdab43c602
siddu-1118 <sv3824@srmist.edu.in>
2026-03-02 14:14:20 +0530

    week 2 code update

---
+++ Program2.java
@@ -0,0 +1,22 @@
+public class Program2 {
+    public static void main(String[] args) {
+
+        int x = 9 + 3 / 2 * 4 - 2;
+        System.out.println(x);
+
+        int y = (9 + 3) / 2 * 4 - 2;
+        System.out.println(y);
+
+        double halfLitre = 500 / 1000;
+        System.out.println(halfLitre + "l");
+
+        halfLitre = (double)500 / 1000;
+        System.out.println(halfLitre + "l");
+
+        double value1 = 20 + 45.68;
+        double value2 = 20 + 45.68;
+        double value3 = 20 + 45.68;
+
+        System.out.println(value1);
+    }
+}
