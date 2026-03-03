ecdaad65181c6a2da568439b44e7dfb20a77126f
siddu-1118 <sv3824@srmist.edu.in>
2026-03-03 13:48:18 +0530

    update 2nd program of 1st week

---
+++ Secondprg.java
@@ -0,0 +1,8 @@
+public class Secondprg {
+	public static void main (String[] args) {
+		int x = 9 + 3 / 2 * 4 - 2;
+		System.out.println(x);
+		int y = (9 + 3) / 2 * 4 - 2;
+		System.out.println(y);
+	}
+}
